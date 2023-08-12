abb78123f02b992846f6015d4da24ab9823bf3c3
mehtabq24 <mehtabq24@gmail.com>
2023-08-12 16:34:58 +0530

    testing

--- include/header.php
+++ include/header.php
@@ -12,7 +12,7 @@ include('include/database.php');
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>House of Sneakers</title>
+    <title>House of Sneakers 1</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -31,10 +31,6 @@ include('include/database.php');
       <base href="<?php echo $actual_link; ?>">
     <?php
       }
-
-
-
-
       setcookie("userID");
       $id = uniqid();
       if(!isset($_COOKIE["userID"])) {
