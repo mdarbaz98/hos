87cfe2d1fcb4cf01f747d316ff71702741e62e9a
mehtabq24 <mehtabq24@gmail.com>
2023-08-12 17:46:26 +0530

    testing

--- include/header.php
+++ include/header.php
@@ -12,7 +12,7 @@ include('include/database.php');
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>House of Sneakers</title>
+    <title>House of Sneakers 1</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
